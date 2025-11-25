5dea69a562355628bfe8380d79803c7e9139ef3b
Harentsoa NOTOAVINA <harentsoa@newmanstech.com>
2025-11-25 14:50:15 +0300

    feat: Introduce new UI components including Card, Row, SearchBar, ThemedText, and PokemonCard.

---
+++ components/ThemedText.tsx
@@ -0,0 +1,45 @@
+import { Colors } from "@/constants/Color"
+import { useThemeColors } from "@/hooks/useThemeColors"
+import { StyleSheet, Text, type TextProps } from "react-native"
+
+const styles = StyleSheet.create({
+    body3: {
+        fontSize: 10,
+        lineHeight: 16
+    },
+    headline: {
+        fontSize: 24,
+        lineHeight: 32,
+        fontWeight: 'bold'
+    },
+    caption: {
+        fontSize: 8,
+        lineHeight: 12
+    },
+    subtitle1: {
+        fontSize: 14,
+        lineHeight: 16,
+        fontWeight: 'bold'
+    },
+    subtitle2: {
+        fontSize: 12,
+        lineHeight: 16,
+        fontWeight: 'bold'
+    },
+    subtitle3: {
+        fontSize: 10,
+        lineHeight: 16,
+        fontWeight: 'bold'
+    },
+})
+
+type IProps = TextProps & {
+    variant?: keyof typeof styles,
+    color?: keyof typeof Colors['light']
+}
+
+export default function ThemedText({variant, color, style, ...rest}: IProps) {
+    const colors = useThemeColors()
+    return <Text style={[styles[variant ?? 'body3'], {color: colors[color ?? 'grayDark']}, style]} {...rest}/>
+}
+
